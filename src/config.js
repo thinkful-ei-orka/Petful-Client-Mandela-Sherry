631cdc5f3dfa5d0b50ff31f95081e7af471dfa8f
M Jones <jonesman2800@gmail.com>
2020-08-27 13:02:00 -0400

    vercel settings

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8000'
-    // API_ENDPOIONT: 'deployed server url here'
+    API_ENDPOINT: 'http://localhost:8000',
+    REACT_APP_API_BASE: process.env.REACT_APP_API_ENDPOINT
 }
