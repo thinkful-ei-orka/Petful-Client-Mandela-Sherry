1b8323faa338719c207ad7814d783a9f16450f3c
Sherry McCarty <sherrydmccarty@gmail.com>
2020-08-27 13:30:25 -0400

    updated for production

---
+++ build/precache-manifest.a1224a834241ec131daf5612cb03afd9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6cb22733c603b58e6896fa034b30890",
+    "url": "/index.html"
+  },
+  {
+    "revision": "643c0bf23cd8ca09d430",
+    "url": "/static/css/main.2dfcd8f8.chunk.css"
+  },
+  {
+    "revision": "34a186b370b16aea16a0",
+    "url": "/static/js/2.06f56fec.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.06f56fec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "643c0bf23cd8ca09d430",
+    "url": "/static/js/main.8b308cb7.chunk.js"
+  },
+  {
+    "revision": "72c5965cbfb3ac6a55d7",
+    "url": "/static/js/runtime-main.40d8ffab.js"
+  },
+  {
+    "revision": "c7b811e4c4549786efe65e0a4c38a7c5",
+    "url": "/static/media/catdog.c7b811e4.jpg"
+  }
+]);
